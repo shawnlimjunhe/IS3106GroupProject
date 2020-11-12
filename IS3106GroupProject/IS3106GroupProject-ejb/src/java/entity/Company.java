c47697dbb41bf956aca19fc2a4aa1f4f226cbdc5
shawnlimjunhe <shawnlimjunhe@gmail.com>
2020-11-12 19:36:06 +0800

    Update to Company

--- IS3106GroupProject/IS3106GroupProject-ejb/src/java/entity/Company.java
+++ IS3106GroupProject/IS3106GroupProject-ejb/src/java/entity/Company.java
@@ -18,97 +18,101 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class Company implements Serializable {
-
+    
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    
     private String name;
     private String username;
     private double balance;
     private String password;
-
+    
     @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
     private List<Post> posts;
-
+    
     @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
     private List<Contract> contracts;
-
+    
     public Company() {
         this.posts = new ArrayList<>();
     }
-
+    
     public Company(String username, String name, double balance) {
         this();
         this.username = username;
         this.name = name;
         this.balance = balance;
     }
-
+    
     public String getUsername() {
         return username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public double getBalance() {
         return balance;
     }
-
+    
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public List<Post> getPosts() {
         return posts;
     }
-
+    
     public void setPosts(List<Post> posts) {
         this.posts = posts;
     }
-
+    
+    public void addPost(Post p) {
+        this.posts.add(p);
+    }
+    
     public List<Contract> getContracts() {
         return contracts;
     }
-
+    
     public void setContracts(List<Contract> contracts) {
         this.contracts = contracts;
     }
-
+    
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
     @Override
     public int hashCode() {
         int hash = 0;
         hash += (id != null ? id.hashCode() : 0);
         return hash;
     }
-
+    
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
@@ -121,10 +125,10 @@ public class Company implements Serializable {
         }
         return true;
     }
-
+    
     @Override
     public String toString() {
         return "entity.Company[ id=" + id + " ]";
     }
-
+    
 }
