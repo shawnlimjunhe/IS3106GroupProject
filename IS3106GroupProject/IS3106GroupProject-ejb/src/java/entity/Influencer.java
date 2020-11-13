5403f99233494767362e84b0898569a1c7b5b7b5
chewzhaoen <e0325712@u.nus.edu>
2020-11-13 23:12:59 +0800

    influencer username unique

--- IS3106GroupProject/IS3106GroupProject-ejb/src/java/entity/Influencer.java
+++ IS3106GroupProject/IS3106GroupProject-ejb/src/java/entity/Influencer.java
@@ -25,7 +25,7 @@ public class Influencer implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
     private String name;
     private String password;
